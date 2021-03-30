4ab9148016efd3355b60375dd91a59247ca91d81
natemoylan <nmoylan@hawaii.edu>
2021-03-30 00:00:15 -1000

    A1

--- Moylan_Nathaniel_Assignment1/server.js
+++ Moylan_Nathaniel_Assignment1/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('.'));
+app.use(express.static('./Public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
